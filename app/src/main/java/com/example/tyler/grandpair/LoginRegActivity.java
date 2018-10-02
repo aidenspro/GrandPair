dc5bf37603f626b145535a4fa66e8725f88fceac
aidenspro <aidenspro@users.noreply.github.com>
2018-10-02 14:03:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tyler/grandpair/LoginRegActivity.java
@@ -0,0 +1,42 @@
+package com.example.tyler.grandpair;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginRegActivity extends AppCompatActivity {
+
+    private Button mLogin, mRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_reg);
+
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginRegActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+            });
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginRegActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        }
+    }
+
