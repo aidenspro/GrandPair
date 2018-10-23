e68dfca37b10140ad55bf40e28d034f546271acd
aidenspro <tyler280>
2018-10-23 17:33:15 -0400

    changes to createProfile page

--- app/src/main/java/com/example/tyler/grandpair/CreateProfile.java
+++ app/src/main/java/com/example/tyler/grandpair/CreateProfile.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
@@ -17,6 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,23 +33,10 @@ public class CreateProfile extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener fbListener;
 
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-        FirebaseApp.initializeApp(this);
-        mAuth = FirebaseAuth.getInstance();
-        fbListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if (user != null) {
-                    Intent intent = new Intent(CreateProfile.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                    return;
-                }
 
-            }
-        };
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_reg);
+        mAuth = FirebaseAuth.getInstance();
         mProfile = (Button) findViewById(R.id.saveProfile);
         mFirstName = (EditText) findViewById(R.id.firstName);
         mLastName = (EditText) findViewById(R.id.lastName);
@@ -58,8 +49,10 @@ public class CreateProfile extends AppCompatActivity {
                 final String lastName = mLastName.getText().toString();
                 final String age = mAge.getText().toString();
 
+
                 String User_id = mAuth.getCurrentUser().getUid();
-                DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child(User_id);
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference current_user_db = db.getInstance().getReference().child("Users").child(User_id);
 
                 Map newPost = new HashMap();
                 newPost.put("first name", firstName);
@@ -68,7 +61,10 @@ public class CreateProfile extends AppCompatActivity {
 
                 current_user_db.setValue(newPost);
 
-                    }
+
+finish();
+
+            }
                 }
 
                 );
