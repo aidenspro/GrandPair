23dcd297f0cc28585e2aee791af0fb63d1dcb87f
aidenspro <aidenspro@users.noreply.github.com>
2018-10-26 02:45:03 -0400

    Can upload images to storage can't quite view profile image yet

--- app/src/main/java/com/example/tyler/grandpair/CreateProfile.java
+++ app/src/main/java/com/example/tyler/grandpair/CreateProfile.java
@@ -1,43 +1,46 @@
 package com.example.tyler.grandpair;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CreateProfile extends AppCompatActivity {
 
-    private Button mProfile;
+    private Button mProfile,mSelect;
     private EditText mFirstName, mLastName, mAge;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener fbListener;
-
+    private int PICK_IMAGE;
+    private String USER_ID;
+    private StorageReference mStorageRef;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_profile_reg);
         mAuth = FirebaseAuth.getInstance();
         mProfile = (Button) findViewById(R.id.saveProfile);
+        mSelect = (Button) findViewById(R.id.selectImage);
         mFirstName = (EditText) findViewById(R.id.firstName);
         mLastName = (EditText) findViewById(R.id.lastName);
         mAge = (EditText) findViewById(R.id.age);
@@ -62,15 +65,72 @@ public class CreateProfile extends AppCompatActivity {
                 current_user_db.setValue(newPost);
 
 
-finish();
+                finish();
 
             }
                 }
 
                 );
+        mSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String firstName = mFirstName.getText().toString();
+                final String lastName = mLastName.getText().toString();
+                final String age = mAge.getText().toString();
 
+                String User_id = mAuth.getCurrentUser().getUid();
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference current_user_db = db.getInstance().getReference().child("Users").child(User_id);
+                USER_ID = User_id;
 
+                pickImage();
 
+                                        }
+                                    }
+
+        );
+    }
+    public void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference mountainsRef = mStorageRef.child(USER_ID + "pic1.jpg");
+        StorageReference mountainImagesRef = mStorageRef.child("images/mountains.jpg");
+        InputStream IS = null;
+
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                //Display an error
+                return;
+            }
+            try {
+                IS = this.getContentResolver().openInputStream(data.getData());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            UploadTask uploadTask = mountainsRef.putStream(IS);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(CreateProfile.this, "Picture Uploaded", Toast.LENGTH_SHORT).show();
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                }
+            });
+
+        }
 
     }
+
 }
--- app/src/main/java/com/example/tyler/grandpair/ProfileActivity.java
+++ app/src/main/java/com/example/tyler/grandpair/ProfileActivity.java
@@ -1,24 +1,84 @@
 package com.example.tyler.grandpair;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private int PICK_IMAGE_REQUEST;
+    private FirebaseAuth mAuth;
+    private Uri mImageUri;
+    private ImageView mImageView;
+    private StorageReference mStorageRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         setContentView(R.layout.activity_profile);
+        mAuth = FirebaseAuth.getInstance();
+        String User_id = mAuth.getCurrentUser().getUid();
+        mImageView =(ImageView)findViewById(R.id.profilePicture);
+        String start = "https://firebasestorage.googleapis.com/v0/b/elderly-social-network.appspot.com/o/";
+        String end = "pic1.jpg?alt=media&token=aabc5f4c-bc6b-4226-b774-82f9bf604dbc";
+
+        mStorageRef.child(User_id + "pic1.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                // Got the download URL for 'users/me/profile.png'
+                Drawable drawable = LoadImageFromWebOperations(uri.toString());
+                try {
+                    URL url = new URL(uri.toString());
+                    Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    mImageView.setImageBitmap(bmp);
+                    //Toast.makeText(ProfileActivity.this, uri.toString(), Toast.LENGTH_SHORT).show();
+                    mImageView.setImageDrawable(drawable);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+    }
+    private Drawable LoadImageFromWebOperations(String url) {
+        try
+        {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+           // mImageView.setImageDrawable(d);
+            return d;
+        }catch (Exception e) {
+            System.out.println("Exc="+e);
+            return null;
+        }
     }
 
 
 
-
-
-
-
 }
+
+
