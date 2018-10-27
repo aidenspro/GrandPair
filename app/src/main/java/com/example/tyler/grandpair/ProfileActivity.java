b8a93d4e55df1ff18470675fdd6349f913248a7b
aidenspro <aidenspro@users.noreply.github.com>
2018-10-27 00:27:11 -0400

    profile page displays image, and information saved by the user

--- app/src/main/java/com/example/tyler/grandpair/ProfileActivity.java
+++ app/src/main/java/com/example/tyler/grandpair/ProfileActivity.java
@@ -1,56 +1,109 @@
 package com.example.tyler.grandpair;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ProfileActivity extends AppCompatActivity {
-    private int PICK_IMAGE_REQUEST;
     private FirebaseAuth mAuth;
-    private Uri mImageUri;
     private ImageView mImageView;
+    private TextView mAge;
+    private TextView mFirstName;
+    private TextView mLastName;
+    private String fName;
+    private String lName;
+    private String age;
     private StorageReference mStorageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mStorageRef = FirebaseStorage.getInstance().getReference();
         setContentView(R.layout.activity_profile);
+        mFirstName = (TextView) findViewById(R.id.firstName);
+        mLastName = (TextView) findViewById(R.id.lastName);
+        mAge = (TextView) findViewById(R.id.age);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
         String User_id = mAuth.getCurrentUser().getUid();
         mImageView =(ImageView)findViewById(R.id.profilePicture);
-        String start = "https://firebasestorage.googleapis.com/v0/b/elderly-social-network.appspot.com/o/";
-        String end = "pic1.jpg?alt=media&token=aabc5f4c-bc6b-4226-b774-82f9bf604dbc";
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        final DatabaseReference getFirst = db.getInstance().getReference().child("Users").child(User_id).child("first name");
+        DatabaseReference getLast = db.getInstance().getReference().child("Users").child(User_id).child("last name");
+        DatabaseReference getAge = db.getInstance().getReference().child("Users").child(User_id).child("age");
+        getFirst.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            fName = dataSnapshot.getValue(String.class);
+                mFirstName.setText(fName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        getLast.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                lName = dataSnapshot.getValue(String.class);
+                mLastName.setText(lName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        getAge.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                age = dataSnapshot.getValue(String.class);
+                mAge.setText(age);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
+
 
         mStorageRef.child(User_id + "pic1.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
                 // Got the download URL for 'users/me/profile.png'
-                Drawable drawable = LoadImageFromWebOperations(uri.toString());
+                //Drawable drawable = LoadImageFromWebOperations(uri.toString());
                 try {
                     URL url = new URL(uri.toString());
-                    Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-                    mImageView.setImageBitmap(bmp);
-                    //Toast.makeText(ProfileActivity.this, uri.toString(), Toast.LENGTH_SHORT).show();
-                    mImageView.setImageDrawable(drawable);
+                    Glide.with(getApplicationContext()).load(uri).into(mImageView);
+
+                    //Toast.makeText(Profitivity.this, uri.toString(), Toast.LENGTH_SHORT).show();
+                    //mImageView.setImageDrawable(drawable);
                 } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
@@ -64,18 +117,7 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
     }
-    private Drawable LoadImageFromWebOperations(String url) {
-        try
-        {
-            InputStream is = (InputStream) new URL(url).getContent();
-            Drawable d = Drawable.createFromStream(is, "src name");
-           // mImageView.setImageDrawable(d);
-            return d;
-        }catch (Exception e) {
-            System.out.println("Exc="+e);
-            return null;
-        }
-    }
+
 
 
 
