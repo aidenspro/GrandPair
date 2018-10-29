1e3f86a5a4d8770e3a9552978667c166bf5fd847
aidenspro <aidenspro@users.noreply.github.com>
2018-10-29 16:19:21 -0400

    changes to profile page and settings

--- app/src/main/java/com/example/tyler/grandpair/SettingsActivity.java
+++ app/src/main/java/com/example/tyler/grandpair/SettingsActivity.java
@@ -1,13 +1,34 @@
 package com.example.tyler.grandpair;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
+
+    private TextView mProfileSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mProfileSettings = (Button) findViewById(R.id.profileSettings);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mProfileSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsActivity.this, CreateProfile.class);
+                startActivity(intent);
+
+                return;
+
+            }
+        });
+
+
     }
+
 }
